21d0aca56fb2fe60851aaa7715d8c5fcb000f164
gersondavidgarcia <gersondavidgarcia@gmail.com>
2026-09-24 00:05:57 -0500

    Add files via upload

--- bingo75/service-worker.js
+++ bingo75/service-worker.js
@@ -2,7 +2,7 @@
    SERVICE WORKER - Bingo Tradicional
    Permite que la app funcione offline y cargue rápido.
    ============================================ */
-const CACHE_NAME = 'bingo-tradicional-v3';
+const CACHE_NAME = 'bingo-tradicional-v2';
 
 // Archivos que se guardan para funcionar sin internet
 const ARCHIVOS_CACHE = [
